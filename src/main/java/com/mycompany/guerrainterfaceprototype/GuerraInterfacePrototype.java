5d2038c5ef83a3c64976423db3c8948a138ea46b
EliasA22 <elias2203@icloud.com>
2025-12-09 20:17:44 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/guerrainterfaceprototype/GuerraInterfacePrototype.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guerrainterfaceprototype;
+
+/**
+ *
+ * @author owenjohnson
+ */
+public class GuerraInterfacePrototype {
+
+    public static void main(String[] args) {
+        MainMenu main = new MainMenu();
+        main.setVisible(true);
+    }
+}
